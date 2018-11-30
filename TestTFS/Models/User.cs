36099e92afc3eabc932673c97defe0a93440e93e
mohammad fazeli <f.mohammadfazeli@gmail.com>
2018-11-30 14:54:04 +0330

    add Age to User

--- TestTFS/Models/User.cs
+++ TestTFS/Models/User.cs
@@ -10,5 +10,7 @@ namespace TestTFS.Models
         public int ID { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
+        public int  Age { get; set; }
+
     }
 }
