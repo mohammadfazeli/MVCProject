ce9171e84f15394680bcdba9ea341a08dd441553
mohammad fazeli <f.mohammadfazeli@gmail.com>
2018-11-30 13:41:45 +0330

    add LastName in User

---
+++ TestTFS/Models/DataBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace TestTFS.Models
+{
+    public class DataBaseContext : DbContext
+
+    {
+        public DataBaseContext() : base("FirstProjectTFS") { }
+
+        public DbSet<User> User { get; set; }
+
+    }
+}
